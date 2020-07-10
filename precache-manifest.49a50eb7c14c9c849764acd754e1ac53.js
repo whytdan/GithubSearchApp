202045ad3b0e80f8f280a0c50d838a68cf6f1800
whytdan <aktan.r.a@gmail.com>
2020-07-11 02:30:10 +0600

    Updates

---
+++ precache-manifest.49a50eb7c14c9c849764acd754e1ac53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f1c760e2aae97a3a500197491898c4",
+    "url": "/GithubSearchApp/index.html"
+  },
+  {
+    "revision": "09a350d2f9946069bf6a",
+    "url": "/GithubSearchApp/static/css/main.9376cb57.chunk.css"
+  },
+  {
+    "revision": "bf3df0cfc87d0dfcb62d",
+    "url": "/GithubSearchApp/static/js/2.7c1205c0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GithubSearchApp/static/js/2.7c1205c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a350d2f9946069bf6a",
+    "url": "/GithubSearchApp/static/js/main.9f7cab3c.chunk.js"
+  },
+  {
+    "revision": "b2d6fd23e4a22da9830e",
+    "url": "/GithubSearchApp/static/js/runtime-main.2b63195b.js"
+  }
+]);
